c75470014438aab4c12fceaa5f09bc6a81fb19ee
Steve <banmart@gmail.com>
2026-07-08 08:49:52 -0700

    Remove missing exports in PageComponents

--- src/components/PageComponents.ssr.tsx
+++ src/components/PageComponents.ssr.tsx
@@ -1,30 +1,17 @@
 export { default as GobiyaLanding } from './GobiyaLanding';
 export { default as GobiyaAboutPage } from './GobiyaAboutPage';
-export { default as ServiceSubpage } from './ServiceSubpage';
-export { default as ArticlePage } from './ArticlePage';
 export { default as AuthorPage } from './AuthorPage';
 export { default as ThankYouPage } from './ThankYouPage';
 export { default as BookingPage } from './BookingPage';
 export { default as SolutionPage } from './SolutionPage';
 export { default as SuccessStories } from './SuccessStories';
-export { default as InsightsPage } from './InsightsPage';
-export { default as RegionalHubPage } from './RegionalHubPage';
 export { default as AdminLogin } from './AdminLogin';
 export { default as AdminDashboard } from './AdminDashboard';
 export { default as ContactPage } from './ContactPage';
-export { default as OnPageSeoLosAngelesPage } from './OnPageSeoLosAngelesPage';
-export { default as AiSeoBeverlyHillsPage } from './AiSeoBeverlyHillsPage';
 
-export { default as LocalSeoBurbankPage } from './LocalSeoBurbankPage';
-export { default as SeoCompanyEncinoPage } from './SeoCompanyEncinoPage';
-export { default as LosAngelesSeoProf } from './LosAngelesSeoProf';
 export { default as SEO } from './SEO';
 export { default as NotFound } from './NotFound';
 
-export { default as GlendaleSeoPage } from './GlendaleSeoPage';
-export { default as PlasticSurgeryMarketingPage } from './PlasticSurgeryMarketingPage';
-export { default as InternetMarketingServicesLosAngelesPage } from './InternetMarketingServicesLosAngelesPage';
-export { default as AiSearchMarketingSantaClaritaPage } from './AiSearchMarketingSantaClaritaPage';
 
 export { default as OutcomesIndex } from './OutcomesIndex';
 export { default as OutcomeTrafficPage } from './OutcomeTrafficPage';
